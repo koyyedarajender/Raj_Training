de4992ede1609bdd2e51e187080e30e1f504c72f
Rajender koyyeda <rkoyyeda@otalio.com>
2022-02-10 20:14:45 +0530

    ProjectStarted

---
+++ Repo/coreJava/ArraysExample.java
@@ -0,0 +1,19 @@
+package com.org.project.coreJava;
+
+/**
+ * Created by rajender.koyyeda on 10-02-2022.
+ */
+public class ArraysExample {
+    public static void main(String[] agrs) {
+       int[] scores = { 215, 234, 218, 189, 221, 290};
+
+        for(int i = 0; i < scores.length; i++)
+        {
+            int score = scores[i];
+            System.out.print(score + "  ");
+
+        }System.out.println();
+    }
+
+
+}
